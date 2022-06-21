ac8224a2c24c546f08bf3bfa64d0e5e8367764a4
codercgx <529488618@qq.com>
2022-06-21 15:52:05 +0800

    深拷贝的实现

--- 01-JavaScript/深拷贝函数/v3-循环引用.js
+++ 01-JavaScript/深拷贝函数/v3-循环引用.js
@@ -3,29 +3,35 @@ function isObject(value) {
   return valueType !== null && (valueType === 'object' || valueType === 'function')
 }
 
-function deepClone(originValue) {
+function deepClone(originValue, map = new WeakMap()) {
   // 判断如果是函数类型, 那么直接使用同一个函数
   if (typeof originValue === 'function') {
     return originValue
   }
 
+  // 不是对象类型，原路返回
   if (!isObject(originValue)) {
     return originValue
   }
 
+  // 如果在 map 中存在，说明循环引用，返回地址即可，不需要克隆
+  if (map.has(originValue)) {
+    return map.get(originValue)
+  }
+
   // 判断传入的对象是数组, 还是对象
   const newObject = Array.isArray(originValue) ? [] : {}
+  // 在 map 存储一份引用
+  map.set(originValue, newObject)
   for (const key in originValue) {
-    newObject[key] = deepClone(originValue[key])
+    // 将 map 作为第二个参数传递进去
+    newObject[key] = deepClone(originValue[key], map)
   }
 
   return newObject
 }
 
 // 测试代码
-let s1 = Symbol("aaa")
-let s2 = Symbol("bbb")
-
 const obj = {
   name: "why",
   age: 18,
@@ -44,4 +50,3 @@ console.log(newObj === obj)
 obj.friend.name = "kobe"
 obj.friend.address.city = "成都"
 console.log(newObj)
-console.log(newObj.s2 === obj.s2)
