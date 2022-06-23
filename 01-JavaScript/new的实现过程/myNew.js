6aff0bce32cacb4e772d3f4c1c3f19a4e1f2952e
codercgx <529488618@qq.com>
2022-06-23 17:51:55 +0800

    new的实现

--- 01-JavaScript/new的实现过程/myNew.js
+++ 01-JavaScript/new的实现过程/myNew.js
@@ -11,3 +11,18 @@ function myNew(fn, ...args) {
   // 4、返回对象
   return obj
 }
+
+
+// 测试代码
+function Person(name, age) {
+  this.name = name
+  this.age = age
+}
+
+const p = new Person('小明', 18)
+console.log(p);
+console.log(p.__proto__);
+
+const p2 = myNew(Person, '小明', 18)
+console.log(p2);
+console.log(p2.__proto__);
