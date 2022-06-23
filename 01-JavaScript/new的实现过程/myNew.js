5158e58ecbad2e49f479a15e480194435454434e
codercgx <529488618@qq.com>
2022-06-23 17:48:23 +0800

    new的实现过程

--- 01-JavaScript/new的实现过程/myNew.js
+++ 01-JavaScript/new的实现过程/myNew.js
@@ -1,5 +1,13 @@
 function myNew(fn, ...args) {
+  // 1、创建一个空对象
   const obj = {}
 
+  // 2、对象的隐式原型指向函数的显示原型
+  obj.__proto__ = fn.prototype
 
+  // 3、执行函数体代码
+  fn.apply(obj, args)
+
+  // 4、返回对象
+  return obj
 }
