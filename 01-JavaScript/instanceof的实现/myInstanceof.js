445d8f772e0be0e2b0f585d5c8a6eb6e84a616ec
codercgx <529488618@qq.com>
2022-06-24 09:05:20 +0800

    instanceof的实现

---
+++ 01-JavaScript/instanceof的实现/myInstanceof.js
@@ -0,0 +1,31 @@
+function myInstanceof(father, child) {
+  // 1、获取父类函数的原型对象
+  const fp = father.prototype
+  // 2、获取子类对象的隐式原型
+  const cp = child.__proto__
+
+  // 3、不断获取子类对象的原型，知道该原型为父类函数的原型对象，返回 true
+  while (cp) {
+    if (cp === fp) {
+      return true
+    }
+
+    cp = cp.__proto__
+  }
+
+  // 4、否则返回 false
+  return false
+}
+
+
+class Person {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+  }
+}
+
+const p = new Person('小明', 18)
+console.log(p instanceof Person);
+
+console.log(myInstanceof(Person, p));
