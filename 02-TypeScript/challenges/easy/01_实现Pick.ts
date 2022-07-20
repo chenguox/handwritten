15c9d0286568e42da8dd902eec57ec1fc16af8e8
codercgx <529488618@qq.com>
2022-07-20 10:46:57 +0800

    typescript

--- 02-TypeScript/challenges/easy/01_实现Pick.ts
+++ 02-TypeScript/challenges/easy/01_实现Pick.ts
@@ -24,16 +24,4 @@ type MyPick<T, K extends keyof T> = {
   [P in K]: T[P]
 }
 
-// 测试
-interface Todo {
-  title: string
-  description: string
-  completed: boolean
-}
 
-type TodoPreview = MyPick<Todo, 'title' | 'completed'>
-
-const todo: TodoPreview = {
-  title: 'Clean room',
-  completed: false,
-}
